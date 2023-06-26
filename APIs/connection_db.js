446186c3b54f72d37cd9162c444d71370b0070b7
carlton435 <runtingchen7@gmail>
2023-06-26 17:59:41 +0800

    finish the connect of the database

--- APIs/connection_db.js
+++ APIs/connection_db.js
@@ -1,1 +1,26 @@
 import mysql from 'mysql'
+const connection = mysql.createConnection({
+    database: 'stickytasks',
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'admin123',
+})
+export default connection
+
+connection.connect((error) => {
+    if (error) {
+        console.error('连接失败：', error);
+    } else {
+        console.log('连接成功！');
+        // 进行其他数据库操作...
+        // connection.query(...)
+        // connection.end();
+    }
+});
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import routers from './routers.js'
-
+import connection from './APIs/connection_db.js'
 const app = express()
 
 app.use('/api', routers)
+
+
 app.listen(3001, () => {
   console.log('server is running on http://127.0.0.1:3001');
 })
